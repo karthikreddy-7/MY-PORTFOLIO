eed183f6312950e3c96c5153a8bce3a032ba7e8f
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-10-12 21:52:33 +0530

    assets

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './Navbar.scss'
+import { images } from '../../constants'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='app_navbar'>
+        <div className='app_navbar-logo'>
+            <img src={images.logo} alt='logo' />
+        </div>
+        <ul className='app_navbar-links'>
+                {['home','about','work','skills','contact'].map((item)=>(
+                    <li className='app__flex p-text' key={'link-${item}'}>
+                        <div/>
+                        <a href={'#${item'}>{item}</a>
+                    </li>
+                ))}
+        </ul>
+    </nav>
   )
 }
 
