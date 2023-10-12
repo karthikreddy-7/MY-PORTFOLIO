d5645ab7b7bfd3a56febc05da1f2aad81db6cdcc
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-10-12 22:29:47 +0530

    frontend

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,19 +1,25 @@
-import React from "react";
-import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
-import { Navbar } from "./components";
-import "./App.scss";
-const App = () => {
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Header />
-      <About />
-      <Work />
-      <Skills />
-      <Testimonial />
-      <Footer />
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
