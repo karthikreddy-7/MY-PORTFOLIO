57f49251a572569efdb845392263f62e92ed6cc2
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-10-14 09:03:33 +0530

    analytics

--- portfolio/App.js
+++ portfolio/App.js
@@ -4,11 +4,14 @@ import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
 import { Navbar } from "./components";
 import "./App.scss";
 import { inject } from "@vercel/analytics";
+import { Analytics } from "@vercel/analytics/react";
 
 inject();
 function App() {
   return (
     <>
+      {inject()}
+      <Analytics />
       <div className="app">
         <Navbar />
         <Header />
